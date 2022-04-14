fd870df07ec978d5da9a4fd8046dfdc486b5492c
Rishon D'Souza <rishonv.dsouza@gmail.com>
2022-04-14 17:31:01 +0400

    Did a bit of level 3
    collectible bug: counts more than once

--- Assets/Scripts/PlayerLife.cs
+++ Assets/Scripts/PlayerLife.cs
@@ -2,21 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerLife : MonoBehaviour
 {
+    private int lives = 3;
     private Rigidbody2D rb;
     private Animator anim;
+    [SerializeField] Text tb;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        tb.text = "Health: " + lives;
     }
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.gameObject.CompareTag("Traps"))
+        if (other.gameObject.CompareTag("Traps") || other.gameObject.CompareTag("Reapable"))
         {
-            Death();
+            lives--;
+            tb.text = "Health: " + lives;
+            if (lives == 0)
+            {
+                Death();
+            }
+
             Debug.Log("You are dead");
         }
     }
