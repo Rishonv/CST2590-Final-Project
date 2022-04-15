d19847c9cc2d3743b520ce27e2f67947c59ac9b7
Rishon D'Souza <rishonv.dsouza@gmail.com>
2022-04-15 13:07:43 +0400

    Level Change broken

--- Assets/Scripts/FInish.cs
+++ Assets/Scripts/FInish.cs
@@ -1,21 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FInish : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
+    [SerializeField] private int nextLevel;
     private void OnTriggerEnter2D(Collider2D other)
     {
-        CompleteLevel();
+        if (other.gameObject.name == "Player")
+        {
+            Invoke("CompleteLevel", 2f);
+        }
     }
 
     private void CompleteLevel()
     {
-
+        SceneManager.LoadScene(nextLevel);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
